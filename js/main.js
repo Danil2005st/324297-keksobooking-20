1872773e5756048a9626b1a475d4b56ec3ca4887
Danila Prokopenko <danila.prokopenko@p2h.com>
2020-06-08 16:40:04 +0300

    fix function name

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ var createData = function () {
 };
 var pinsData = createData();
 
-var insertPin = function (pin) {
+var createPin = function (pin) {
   var newPin = mapPin.cloneNode(true);
   var img = newPin.querySelector('img');
   newPin.style.left = pin.location.x - PIN_WIDTH / 2 + 'px';
@@ -59,14 +59,14 @@ var insertPin = function (pin) {
   return newPin;
 };
 
-var createPinList = function (pins) {
+var insertPinList = function (pins) {
   var pinsList = document.createDocumentFragment();
 
   for (var i = 0; i < pins.length; i++) {
-    pinsList.appendChild(insertPin(pins[i]));
+    pinsList.appendChild(createPin(pins[i]));
   }
 
   mapPins.appendChild(pinsList);
 };
 
-createPinList(pinsData);
+insertPinList(pinsData);
